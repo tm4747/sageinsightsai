2f94a14b03d4f27e559092e99e215587078d56f9
tm4747 <tm4747@hotmail.com>
2025-05-31 09:46:15 -0400

    refactor most styles except for character configurator

--- src/components/AILogo.js
+++ src/components/AILogo.js
@@ -1,10 +1,10 @@
 import logo from '../logo-gear.png';
-import './styles/AILogo.css';
+import styles from './styles/AILogo.module.css';
 
 const AILogo = ({size, transparency}) => {
   return (
   <>
-  <img src={logo} className={"App-logo"} alt="logo" style={{height:size}} />
+  <img src={logo} className={styles.AppLogo} alt="logo" style={{height:size}} />
   </>
   )
 }
