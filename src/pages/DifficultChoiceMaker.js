1fe11af219c35dfdeede523f59f41004aaefd642
tm4747 <tm4747@hotmail.com>
2025-06-01 10:01:46 -0400

    begin on difficult choice maker

--- src/pages/DifficultChoiceMaker.js
+++ src/pages/DifficultChoiceMaker.js
@@ -3,19 +3,29 @@ import styles from './css/PageStyles.module.css';
 import BoxList from '../components/BoxList';
 import { getDifficultChoiceMakerHowItWorks } from '../lib/DataHelper';
 import PageDescription from '../components/PageDescription';
-// todo: test - remove
+import FlashingText from '../components/FlashingText';
 
 function DifficultChoiceMaker({setIsLoading}) {
   const [showBoxList, setShowBoxList] = useState(false);
+  const [choiceText, setChoiceText] = useState("");
+  const [choiceTextDone, setChoiceTextDone] = useState(false);
 
 
   /********* USE EFFECTS & API CALLS **********/
 
 
   /********** DYNAMIC JS FUNCTIONS **********/ 
-
   const handleShowHowItWorks = () => {
     setShowBoxList(!showBoxList);
+  }
+
+  const resetState = () => {
+    setChoiceText("");
+    setChoiceTextDone(false);
+  }
+
+  const addCriteria = () => {
+    // add criteria row
   }
 
   /***********HELPER FUNCTIONS ************/
@@ -27,6 +37,15 @@ function DifficultChoiceMaker({setIsLoading}) {
   const pageDescText = "This tool will assist in making difficult and/or complex choices:";
   const descriptionOfPageFunction = <PageDescription onClickFn={handleShowHowItWorks} text={pageDescText} />
 
+  const decisionInput = !choiceTextDone ? (<><label>Describe the decision or choice:</label>
+    <input className={"text-input"} value={choiceText} type="text" onChange={(e) => setChoiceText(e.target.value)}/></>) : "";
+  const decisionGoodButton = !choiceTextDone ? <button className={"button green-button"} onClick={() => {setChoiceTextDone(true)}} value={""}>Decision is correct!</button> : "";
+  const startOverButton = choiceTextDone ? <button className={"button red-button"} onClick={resetState} value={""}>Start Over</button> : "";
+  const addChoiceCriteriaButton = choiceTextDone ? 
+  <><button className={"button green-button"} onClick={addCriteria} value={""}>Add Criteria</button>
+    <span> - these are factors of the decision you will use in evaluation.</span>
+  </>  : "";
+
   return (
     <div className={styles.content}>
       <div className={"formDiv"}>
@@ -37,7 +56,13 @@ function DifficultChoiceMaker({setIsLoading}) {
       </div>
       <div className={"resultsDiv"} >
         <div className={"innerResultsDiv"}>
-          
+          {startOverButton}
+          {decisionInput}
+          <p>
+            <FlashingText text={choiceText}/>
+          </p>
+          {decisionGoodButton} 
+          {addChoiceCriteriaButton}
         </div>
       </div>
     </div>
