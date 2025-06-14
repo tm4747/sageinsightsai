7417d317f1b2e6e3663dd49d4c28bfaeea85931d
tm4747 <tm4747@hotmail.com>
2025-06-14 16:17:14 -0400

    fade-in on view change

--- src/Layout.js
+++ src/Layout.js
@@ -1,5 +1,5 @@
 // src/Layout.js
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Outlet, Link, useLocation } from 'react-router-dom';
 import AILogo from './components/AILogo';
 import LoadingModal from "./components/LoadingModal";
@@ -8,20 +8,36 @@ import TypingText from './components/TypingText';
 const Layout = ({isLoading}) => {
   const location = useLocation();
   const path = location.pathname;
-  const [userName, setUserName] = useState("");
-  const [validUserNameSubmitted, setValidUserNameSubmitted] = useState(false);
+  const [fadeClass, setFadeClass] = useState("fade-wrapper");
+  const [userName, setUserName] = useState("test - remove and set NL false");
+  const [validUserNameSubmitted, setValidUserNameSubmitted] = useState(true);
   const [nameError, setNameError] = useState(false);
   const [nameErrorMessage, setNameErrorMessage] = useState('');
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => setMenuOpen(prev => !prev);
 
 
+  /***** USE EFFECTS  ******/
+  useEffect(() => {
+    setFadeClass("fade-enter"); // Start at opacity 0
+
+    const timeout = setTimeout(() => {
+       // Force reflow before setting class to trigger transition
+      setFadeClass("fade-wrapper"); // triggers fade-in animation
+    }, 200); // Small delay to trigger CSS transition
+    return () => clearTimeout(timeout);
+  }, [location.pathname]);
+
+
+
   const linkData = [
-    { path: '/', label: 'Web Summary' },
-    { path: '/story-maker', label: 'Story Maker' },
-    { path: '/decidedly-choice-tool', label: 'Decidedly' },
+    { path: '/', label: 'Web Summary', active: path === '/' },
+    { path: '/story-maker', label: 'Story Maker', active: path === '/story-maker' },
+    { path: '/decidedly-choice-tool', label: 'Decidedly', active: path === '/decidedly-choice-tool' },
   ];
-  
+  const activeLink = linkData.find(link => link.active);
+  const activeLinkText = activeLink?.label;
+
 
   const nav = (
     <>
@@ -31,7 +47,7 @@ const Layout = ({isLoading}) => {
           <React.Fragment key={link.path}>
             {index > 0 && <span>&nbsp;|&nbsp;</span>}
             <Link
-              className={path === link.path ? 'activeLink' : ''}
+              className={link.active ? 'activeLink' : ''}
               to={link.path}
             >
               {link.label}
@@ -42,14 +58,14 @@ const Layout = ({isLoading}) => {
 
       {/* Mobile Hamburger */}
 <div className="mobile-nav">
-  link data for path <button className="hamburger" onClick={toggleMenu}>
+  <span className="bold">{activeLinkText} </span> <button className="hamburger" onClick={toggleMenu}>
      ☰
   </button>
   <div className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
     {linkData.map(link => (
       <Link
         key={link.path}
-        className={`mobile-link ${path === link.path ? 'activeLink' : ''}`}
+        className={`mobile-link ${link.active ? 'activeLink' : ''}`}
         to={link.path}
         onClick={() => setMenuOpen(false)}
       >
@@ -80,22 +96,24 @@ const handleUpdateName = () => {
   const nameInputClasses = nameError ? `text-input red-border` : `text-input`;
 
   if(!validUserNameSubmitted){
-    return(<div className="layout">
-      <main>
-        <div className="App">
-          <header className="App-header">
-            <h3 className={"pageTitle"}>
-            <AILogo size={".75em"}/>
-              <TypingText text={"Please enter your name"} flashingText={"_ "} headerSize={"small"}/>
-            </h3>
-            <input type="text" className={nameInputClasses} value={userName}
-            onChange={(e) => setUserName(e.target.value)} style={{maxWidth: "400px"}}/>
-            {nameErrorText}
-            <button className={"button green-button"} onClick={handleUpdateName} value={"Submit"} style={{margin:0, marginTop:'2rem', width:"100%",maxWidth: "400px"}}>Submit</button>
-         </header>
-        </div>
-      </main>
-    </div>);
+    return(
+      <div className="layout">
+        <main>
+          <div className="App">
+            <header className="App-header">
+              <h3 className={"pageTitle"}>
+              <AILogo size={".75em"}/>
+                <TypingText text={"Please enter your name"} flashingText={"_ "} headerSize={"small"}/>
+              </h3>
+              <input type="text" className={nameInputClasses} value={userName}
+              onChange={(e) => setUserName(e.target.value)} style={{maxWidth: "400px"}}/>
+              {nameErrorText}
+              <button className={"button green-button"} onClick={handleUpdateName} value={"Submit"} style={{margin:0, marginTop:'2rem', width:"100%",maxWidth: "400px"}}>Submit</button>
+          </header>
+          </div>
+        </main>
+      </div>
+    );
   }
 
   return (
@@ -109,7 +127,9 @@ const handleUpdateName = () => {
             </h2>
             {nav}
             <section className="body">
-              <Outlet /> {/* This renders the current child route */}
+              <div className={fadeClass}>
+                <Outlet /> {/* This renders the current child route */}
+              </div>
             </section>
          </header>
           <LoadingModal isLoading={isLoading}/>
